0a3d3a1fb9196a14c3ec15cfaa3c95dd6cc1b271
Nemtrack <balazsgyenes10@gmail.com>
2024-02-25 12:51:35 +0100

    Add elementary user authentication logic

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+  isUserAuthenticated = true;
+  userName = 'Balázs';
 }
