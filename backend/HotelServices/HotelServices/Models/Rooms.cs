ae9f8a570531ebcda9581c0f380083514ff27efa
dpatrik0610 <d.patrik0610@gmail.com>
2024-02-25 16:46:48 +0100

    Defined the model of the rooms.

---
+++ backend/HotelServices/HotelServices/Models/Rooms.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+namespace HotelServices.Models
+{
+    public class Rooms
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public int RoomNumber { get; set; }
+        public bool Availability { get; set; } = true;
+        public int Price { get; set; }
+        public RoomType RoomType { get; set; }
+        public string? Description { get; set; }
+        public List<string>? Amenities { get; set; }
+        public List<string>? images { get; set; }
+
+    }
+}
