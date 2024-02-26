728abe2c50c8e1496b00cb12d90e8b757314673d
Nemtrack <balazsgyenes10@gmail.com>
2024-02-26 21:02:53 +0100

    Modification for testing HeaderComponent

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import path from 'path';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: WelcomePageComponent },
   { path: 'about', component: AboutUsComponent },
   { path: 'contact', component: ContactUsComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: '**', redirectTo: '/home' },
 ];
 
 @NgModule({
