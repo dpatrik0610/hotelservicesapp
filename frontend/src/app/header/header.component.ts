c3b52ec950668fd7059f414bbf5a69171e7a598d
Nemtrack <balazsgyenes10@gmail.com>
2024-02-27 22:23:31 +0100

    Implement switching off login button if user is on login page

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, Host, HostListener, OnInit } from '@angular/core';
 import { UserService } from '../shared/user.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +11,7 @@ export class HeaderComponent implements OnInit {
   isUserAuthenticated = false;
   userName = 'Balázs';
   navbarFixed = false;
+  isLoginPage = false;
 
   @HostListener('window:scroll', ['$event']) onScroll() {
     if (window.scrollY > 100) {
@@ -19,9 +21,14 @@ export class HeaderComponent implements OnInit {
     }
   }
 
-  constructor(private user: UserService) {}
+  constructor(private user: UserService, private route: ActivatedRoute) {}
+
   ngOnInit(): void {
     this.isUserAuthenticated = this.user.isUserLoggedIn;
+
+    this.route.url.subscribe((url) => {
+      this.isLoginPage = url[0].path === 'login';
+    });
   }
 
   onLog() {
