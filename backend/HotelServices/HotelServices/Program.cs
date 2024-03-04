e415e49100f3abfd8f8b64efe59d0347781d9f70
dpatrik0610 <d.patrik0610@gmail.com>
2024-03-04 23:40:09 +0100

    Revert "Created RoomController and used the according service functions."
    This reverts commit 05929ec73482e033bca25bb6031670d512e8d67c.

--- backend/HotelServices/HotelServices/Program.cs
+++ backend/HotelServices/HotelServices/Program.cs
@@ -1,33 +1,6 @@
-using HotelServices.Database;
-using HotelServices.Services.Interfaces;
-using HotelServices.Services;
-using Microsoft.Extensions.DependencyInjection;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSingleton<IMongoDatabaseProvider>(provider => { 
-    var config = provider.GetRequiredService<IConfiguration>();
-    var connectionString = config.GetValue<string>("MongoDB:ConnectionURI");
-    var databaseName = config.GetValue<string>("MongoDB:DatabaseName");
-
-    if (string.IsNullOrEmpty(connectionString) || string.IsNullOrEmpty(databaseName))
-    {
-        // Log or throw an exception indicating missing or invalid configuration.
-        throw new ApplicationException("MongoDB connection settings are missing or invalid.");
-    }
-
-    try
-    {
-        return new MongoDatabaseProvider(connectionString, databaseName);
-    }
-    catch (Exception ex)
-    {
-        // Log or throw an exception indicating failure to establish connection.
-        throw new ApplicationException("Failed to establish connection to the MongoDB server.", ex);
-    }
-});
-
-builder.Services.AddSingleton<IRoomService, RoomService>();
+// Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
