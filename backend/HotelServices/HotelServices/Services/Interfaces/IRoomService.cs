ee9a2ce4104394075a0f8711d9f5a325c66bda26
dpatrik0610 <d.patrik0610@gmail.com>
2024-03-04 22:56:13 +0100

    Added new Interface for room services.

---
+++ backend/HotelServices/HotelServices/Services/Interfaces/IRoomService.cs
@@ -0,0 +1,14 @@
+﻿using HotelServices.Models;
+
+namespace HotelServices.Services.Interfaces
+{
+    public interface IRoomService
+    {
+        Task<Room> GetRoomByNumberAsync(int roomNumber);
+        Task<List<Room>> GetAllRoomsAsync();
+        Task<List<Room>> GetAvailableRoomsAsync();
+        Task AddRoomAsync(Room room);
+        Task<bool> UpdateRoomAsync(int roomNumber, Room room);
+        Task<bool> DeleteRoomAsync(int roomNumber);
+    }
+}
