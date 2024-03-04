9b2e1dabab4976a6d16fc2973fe0737b8e573e2a
dpatrik0610 <d.patrik0610@gmail.com>
2024-03-04 22:56:56 +0100

    Removed mongodbconfig and replaced with IMongoDatabase, which is more efficient.

---
+++ backend/HotelServices/HotelServices/Database/IMongoDatabase.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Driver;
+
+namespace HotelServices.Database
+{
+    public interface IMongoDatabaseProvider
+    {
+        IMongoDatabase GetDatabase();
+    }
+
+    public class MongoDatabaseProvider : IMongoDatabaseProvider
+    {
+        private readonly IMongoClient _client;
+        private readonly string _databaseName;
+        public MongoDatabaseProvider(string connectionString, string databaseName)
+        {
+            _client = new MongoClient(connectionString);
+            _databaseName = databaseName;
+        }
+
+        public IMongoDatabase GetDatabase()
+        {
+            return _client.GetDatabase(_databaseName);
+        }
+    }
+}
