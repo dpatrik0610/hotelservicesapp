1a98bd8303838a552cc5f9b5bb966fe084b04e75
dpatrik0610 <d.patrik0610@gmail.com>
2024-03-04 22:57:55 +0100

    Implemented IRoomService.

---
+++ backend/HotelServices/HotelServices/Services/RoomService.cs
@@ -0,0 +1,96 @@
+﻿using HotelServices.Database;
+using HotelServices.Models;
+using HotelServices.Services.Interfaces;
+using MongoDB.Driver;
+
+namespace HotelServices.Services
+{
+    public class RoomService : IRoomService
+    {
+        private readonly IMongoCollection<Room> _roomCollection;
+        public RoomService(IMongoDatabaseProvider databaseProvider) 
+        {
+            var database = databaseProvider.GetDatabase();
+            _roomCollection = database.GetCollection<Room>("Rooms");
+        }
+
+        public async Task<Room> GetRoomByNumberAsync(int roomNumber)
+        {
+            try
+            {
+                return await _roomCollection.Find(room => room.RoomNumber == roomNumber).FirstOrDefaultAsync();
+            }
+            catch (Exception ex)
+            {
+                // Implement logger here.
+                throw new Exception("An error occurred while fetching room data.", ex);
+            }
+        }
+        public async Task<List<Room>> GetAllRoomsAsync()
+        {
+            try
+            {
+                return await _roomCollection.Find(_ => true).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                // Implement logger here.
+                throw new Exception("An error occurred while fetching all rooms.", ex);
+            }
+        }
+
+        public async Task<List<Room>> GetAvailableRoomsAsync()
+        {
+            try
+            {
+                return await _roomCollection.Find(room => room.Availability).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                // Implement logger here.
+                throw new Exception("An error occurred while fetching available rooms.", ex);
+            }
+        }
+
+        public async Task AddRoomAsync(Room room)
+        {
+            try
+            {
+                await _roomCollection.InsertOneAsync(room);
+            }
+            catch (Exception ex)
+            {
+                // Implement logger here.
+                throw new Exception("An error occurred while adding the room.", ex);
+            }
+        }
+
+        public async Task<bool> DeleteRoomAsync(int roomNumber)
+        {
+            try
+            {
+                var result = await _roomCollection.DeleteOneAsync(room => room.RoomNumber == roomNumber);
+                return result.IsAcknowledged && result.DeletedCount > 0;
+            }
+            catch (Exception ex)
+            {
+                // Implement logger here.
+                throw new Exception("An error occurred while deleting the room.", ex);
+            }
+        }
+
+        public async Task<bool> UpdateRoomAsync(int roomNumber, Room newRoomData)
+        {
+            try
+            {
+                var result = await _roomCollection.ReplaceOneAsync(roomToFind => roomToFind.RoomNumber == roomNumber, newRoomData);
+                return result.IsAcknowledged && result.ModifiedCount > 0;
+            }
+            catch (Exception ex)
+            {
+                // Implement logger here.
+                throw new Exception("An error occurred while updating the room's data.", ex);
+            }
+        }
+    }
+}
