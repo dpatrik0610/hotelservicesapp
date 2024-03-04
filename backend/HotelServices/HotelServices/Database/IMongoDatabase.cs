79a9ebf9c7f79a12c19ecafb8879e7b4c292cfcf
dpatrik0610 <d.patrik0610@gmail.com>
2024-03-04 23:37:21 +0100

    added try-catch to MongoDatabaseProvider

--- backend/HotelServices/HotelServices/Database/IMongoDatabase.cs
+++ backend/HotelServices/HotelServices/Database/IMongoDatabase.cs
@@ -13,13 +13,29 @@ namespace HotelServices.Database
         private readonly string _databaseName;
         public MongoDatabaseProvider(string connectionString, string databaseName)
         {
+            if (string.IsNullOrEmpty(connectionString) || string.IsNullOrEmpty(databaseName))
+            {
+                throw new ArgumentException("Connection string or database name cannot be null or empty.");
+            }
+
             _client = new MongoClient(connectionString);
             _databaseName = databaseName;
         }
 
         public IMongoDatabase GetDatabase()
         {
-            return _client.GetDatabase(_databaseName);
+            try
+            {
+                // Check if the MongoDB server is reachable
+                _client.ListDatabaseNames();
+
+                return _client.GetDatabase(_databaseName);
+            }
+            catch (MongoException ex)
+            {
+                // TODO: Implement log here.
+                throw new Exception("Failed to connect to the MongoDB server.", ex);
+            }
         }
     }
 }
