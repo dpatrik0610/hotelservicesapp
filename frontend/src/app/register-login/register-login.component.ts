d3a5d5bd6ccdc6a9a8228ba6635cce088aede372
Nemtrack <balazsgyenes10@gmail.com>
2024-03-09 13:30:33 +0100

    Add validators with stylized warning message on RegisterLoginComponent

--- frontend/src/app/register-login/register-login.component.ts
+++ frontend/src/app/register-login/register-login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-login',
@@ -13,8 +13,8 @@ export class RegisterLoginComponent {
 
   ngOnInit() {
     this.loginForm = new FormGroup({
-      username: new FormControl(null),
-      password: new FormControl(null),
+      username: new FormControl(null, Validators.required),
+      password: new FormControl(null, Validators.required),
     });
   }
 
