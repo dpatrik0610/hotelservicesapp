4354aa93f7a3421064f6dc4634f60a6320cf683f
dpatrik0610 <d.patrik0610@gmail.com>
2024-04-06 13:17:32 +0200

    Serialize RoomType enum as strings for API requests/responses

--- backend/HotelServices/HotelServices/Models/RoomType.cs
+++ backend/HotelServices/HotelServices/Models/RoomType.cs
@@ -1,10 +1,18 @@
-﻿namespace HotelServices.Models
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace HotelServices.Models
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum RoomType
     {
+        [EnumMember(Value = "Standard")]
         Standard,
+        [EnumMember(Value = "Comfort")]
         Comfort,
+        [EnumMember(Value = "Comfort+")]
         ComfortPlus,
+        [EnumMember(Value = "Presidential")]
         Presidential
     }
 }
