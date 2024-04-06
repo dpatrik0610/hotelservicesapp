485423c0ba4771d207a0bb975155ea2988495ea0
dpatrik0610 <d.patrik0610@gmail.com>
2024-04-06 14:23:48 +0200

    Added GetRoomsByNumbersAsync and AddRoomsAsync

--- backend/HotelServices/HotelServices/Services/Interfaces/IRoomService.cs
+++ backend/HotelServices/HotelServices/Services/Interfaces/IRoomService.cs
@@ -5,9 +5,11 @@ namespace HotelServices.Services.Interfaces
     public interface IRoomService
     {
         Task<Room> GetRoomByNumberAsync(int roomNumber);
+        Task<List<Room>> GetRoomsByNumbersAsync(List<int> roomNumbers);
         Task<List<Room>> GetAllRoomsAsync();
         Task<List<Room>> GetAvailableRoomsAsync();
         Task AddRoomAsync(Room room);
+        Task AddRoomsAsync(List<Room> rooms);
         Task<bool> UpdateRoomAsync(int roomNumber, Room room);
         Task<bool> DeleteRoomAsync(int roomNumber);
     }
--- backend/HotelServices/HotelServices/Services/RoomService.cs
+++ backend/HotelServices/HotelServices/Services/RoomService.cs
@@ -2,6 +2,7 @@
 using HotelServices.Models;
 using HotelServices.Services.Interfaces;
 using MongoDB.Driver;
+using System.Runtime.InteropServices;
 
 namespace HotelServices.Services
 {
@@ -94,6 +95,54 @@ namespace HotelServices.Services
             {
                 _logger.LogError(ex, "An error occurred while updating the room's data for room number {RoomNumber}.", roomNumber);
                 throw new Exception("An error occurred while updating the room's data.", ex);
+            }
+        }
+
+        public async Task<List<Room>> GetRoomsByNumbersAsync(List<int> roomNumbers)
+        {
+            try
+            {
+
+                var tasks = roomNumbers.Select(async roomNumber =>
+                {
+                    return await _roomCollection.Find(room => room.RoomNumber == roomNumber).FirstOrDefaultAsync();
+                }).ToList();
+
+                var rooms = await Task.WhenAll(tasks);
+                return rooms.ToList();
+            }
+            catch (Exception ex)
+            {
+                var roomNumbersString = string.Join(", ", roomNumbers);
+                _logger.LogError(ex, "An error occurred while getting rooms with numbers: {RoomNumbers}.", roomNumbersString);
+                throw new Exception($"An error occurred while getting rooms with numbers: {roomNumbersString}. See logs for details.", ex);
+            }
+        }
+
+        public async Task AddRoomsAsync(List<Room> rooms)
+        {
+            try { 
+                var existingRoomNumbers = await _roomCollection
+                    .Find(room => rooms.Select(r => r.RoomNumber).Contains(room.RoomNumber))
+                    .Project(room => room.RoomNumber)
+                    .ToListAsync();
+
+                if (existingRoomNumbers.Any())
+                {
+                    throw new InvalidOperationException($"Rooms already exist with the following room numbers: {string.Join(", ", existingRoomNumbers)}");
+                }
+
+                await _roomCollection.InsertManyAsync(rooms);
+            }
+            catch (InvalidOperationException ex)
+            {
+                _logger.LogError(ex.Message);
+                throw new InvalidOperationException(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while Adding rooms.");
+                throw new Exception("An error occurred while Adding rooms.");
             }
         }
     }
