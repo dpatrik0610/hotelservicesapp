407f2462b6991233e5d42d7ee8083d1da8c4cc31
dpatrik0610 <d.patrik0610@gmail.com>
2024-04-07 17:58:45 +0200

    Added new models.

---
+++ backend/HotelServices/HotelServices/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelServices.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [Required(ErrorMessage = "Nickname is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Nickname must be between 3 and 50 characters")]
+        public string? Nickname { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters")]
+        public string? Password { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime RegDate { get; } = DateTime.UtcNow;
+    }
+}
---
+++ backend/HotelServices/HotelServices/Models/UserReservation.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelServices.Models
+{
+    public class UserReservation
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [Required(ErrorMessage = "User ID is required")]
+        public required string UserId { get; set; }
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [Required(ErrorMessage = "Room ID is required")]
+        public string? RoomId { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime ReservationDate { get; } = DateTime.UtcNow;
+    }
+}
