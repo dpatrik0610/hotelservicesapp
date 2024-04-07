da59c06e75c5c6f3920eddd607ce0400c9dccc94
Nemtrack <balazsgyenes10@gmail.com>
2024-04-07 18:45:15 +0200

    Shared Rooms application-wide with renaming

--- frontend/src/app/rooms/rooms.component.ts
+++ frontend/src/app/rooms/rooms.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
 import { Subscription } from 'rxjs';
 import { Room } from '../shared/room.model';
-import { CurrentRoomService } from '../shared/current-room.service';
+import { RoomService } from '../shared/current-room.service';
 
 @Component({
   selector: 'app-rooms',
@@ -17,7 +17,7 @@ export class RoomsComponent implements OnInit, OnDestroy {
 
   constructor(
     private http: DataStorageService,
-    private currentRoom: CurrentRoomService
+    private roomService: RoomService
   ) {}
   ngOnInit(): void {
     this.getRoomsSubscribtion = this.http.getRooms().subscribe((data) => {
@@ -29,17 +29,21 @@ export class RoomsComponent implements OnInit, OnDestroy {
           }
         });
       });
+      this.roomService.rooms = this.rooms;
       console.log(this.rooms);
     });
 
     // this.getRoomSubscribtion = this.http.getRoom(101).subscribe((data) => {
     //   console.log(data);
     // });
-    this.isLoading = false;
+
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 1000);
   }
 
   onSetRoom(item: Room) {
-    this.currentRoom.currentRoom = item;
+    this.roomService.currentRoom = item;
   }
 
   ngOnDestroy(): void {
--- frontend/src/app/shared/current-room.service.ts
+++ frontend/src/app/shared/current-room.service.ts
@@ -4,7 +4,8 @@ import { Room } from './room.model';
 @Injectable({
   providedIn: 'root',
 })
-export class CurrentRoomService {
+export class RoomService {
+  private _rooms: Room[] = [];
   private _currentRoom: Room = new Room(
     101,
     true,
@@ -18,8 +19,17 @@ export class CurrentRoomService {
   public get currentRoom(): Room {
     return this._currentRoom;
   }
+
+  public get rooms(): Room[] {
+    return this._rooms;
+  }
+
   public set currentRoom(value: Room) {
     this._currentRoom = value;
+  }
+
+  public set rooms(value: Room[]) {
+    this._rooms = value;
   }
 
   constructor() {}
