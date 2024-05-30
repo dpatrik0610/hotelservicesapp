c581ceb302664fb8741cb633831618b6e592ee26
Patrik Dobos <d.patrik0610@gmail.com>
2024-05-30 18:35:05 +0200

    Shared cookie configs

---
+++ backend/HotelServices/HotelServices.Shared/Configurations/CookieConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Linq;
+
+
+namespace HotelServices.Shared.Configurations
+{
+    public static class CookieConfiguration
+    {
+        public static void ConfigureAuthenticationCookie(CookieBuilder options)
+        {
+            var configJson = JObject.Parse(File.ReadAllText("cookieSettings.json"));
+
+            options.Name = (string)configJson["Name"];
+            options.Domain = (string)configJson["Domain"];
+            options.Path = (string)configJson["Path"];
+            options.HttpOnly = (bool)configJson["HttpOnly"];
+            options.SecurePolicy = Enum.Parse<CookieSecurePolicy>((string)configJson["SecurePolicy"]);
+            options.SameSite = Enum.Parse<SameSiteMode>((string)configJson["SameSite"]);
+        }
+    }
+}
